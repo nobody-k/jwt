96b5f787c8b756bfd4c1b79c1522740bce30f47f
nobody-k <bernkarel@gmail.com>
2018-06-21 23:34:30 +0200

    sign method completed

--- jwt.go
+++ jwt.go
@@ -1,18 +1,20 @@
 package jwt
 
 import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/json"
 	"time"
 )
 
-// The approach
-
-// Creating the header
-
-// creating the payload
-
-// creating the signature
-
-// add all three part together
+// ComputeHmac256 creates a hash based on the provided secret
+func ComputeHmac256(message string, secret string) string {
+	key := []byte(secret)
+	h := hmac.New(sha256.New, key)
+	h.Write([]byte(message))
+	return base64.StdEncoding.EncodeToString(h.Sum(nil))
+}
 
 // Claims is the structure of claims
 type Claims map[string]interface{}
@@ -23,20 +25,27 @@ func (c Claims) SetClaim(claim string, v interface{}) Claims {
 	return c
 }
 
-// Sign for the defined claims, takes the secretkey for hasing,
+// Sign produces the token for the defined claims, takes the secretkey for hasing,
 // and expiration in seconds for the exp Claim as defined in the RFC
 // if experation == 0 no exp Claim will be created
-func (c Claims) Sign(alg func(), secretKey string, expiration int) string {
+// at this moment assuming only one method for hashing
+func (c Claims) Sign(secretKey string, expiration int) string {
 	/*
-	* the CLAIMS
+	**************** the HEADER ********************
+	 */
+	// using fized header for now
+	header := base64.StdEncoding.EncodeToString([]byte(`{"alg":"HS256","typ":"JWT"}`))
+	/*
+	**************** the CLAIMS ********************
 	 */
 	// 4.1.1.  "iss" (Issuer) Claim - NOT using for NOW
 	// 4.1.2.  "sub" (Subject) Claim - NOT using for NOW
 	// 4.1.3.  "aud" (Audience) Claim - NOT using for NOW
 	// 4.1.4.  "exp" (Expiration Time) Claim
 	currentTime := time.Now()
-	exp := currentTime.Add(time.Second * time.Duration(expiration))
+
 	if expiration > 0 {
+		exp := currentTime.Add(time.Second * time.Duration(expiration)).Unix()
 		c.SetClaim("exp", exp)
 	}
 	// 4.1.5.  "nbf" (Not Before) Claim - NOT Using for now
@@ -45,4 +54,21 @@ func (c Claims) Sign(alg func(), secretKey string, expiration int) string {
 	c.SetClaim("iat", currentTime.Unix())
 	//4.1.7.  "jti" (JWT ID) Claim - NOT using now
 
+	// assuming other public claims has been set
+	// convert to json
+	j, err := json.Marshal(c)
+	if err != nil {
+		panic(err)
+	}
+	// and encode the payload
+	payload := base64.StdEncoding.EncodeToString(j)
+	/*
+	**************** the Signature ********************
+	 */
+	signature := ComputeHmac256(header+"."+payload, secretKey)
+
+	/*
+	**************** the JWT ********************
+	 */
+	return header + "." + payload + "." + signature
 }
